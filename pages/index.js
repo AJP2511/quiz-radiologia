3c4e5c99fcb1653bb8669ad34d0af14cb0a28fc8
AJP2511 <johnson2511@gmail.com>
2021-01-27 13:49:05 -0300

    nome inicial automatico

--- pages/index.js
+++ pages/index.js
@@ -69,6 +69,12 @@ export default function Home() {
     router.push("/quiz");
   }
 
+  React.useEffect(() => {
+    if (window.localStorage.getItem("jogador")) {
+      setNome(window.localStorage.getItem("jogador"));
+    }
+  }, []);
+
   return (
     <QuizBackground backgroundImage={db.bg}>
       <QuizContainer>
